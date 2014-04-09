8c68cc065fa8a306e546877f5f7aaf2b420ad237
Steven <steven@steven-Inspiron-546.(none)>
2014-04-09 07:21:50 -0500

    Tweaked new11 and added cmdln params for sendmail-wrapper
    Merged two different versions of new11.pl together into one and
    added command line arguments to sendmail-wrapper.  Also changed
    the "Sending" address and added a final confirmation check before
    the message is sent.

--- new11.pl
+++ new11.pl
@@ -8,6 +8,8 @@
 
 #should be able to specify users, terminals, or pids
 
+#things to think about: killing someone else who is root?
+
 use strict;
 use warnings;
 
@@ -25,6 +27,9 @@ my @users;
 my %userdata;
 my @userpids;
 my $mytty;
+
+my $cmd_choice;
+my @killlist;
 
 ################################
 # EXECUTE W CMD
@@ -63,10 +68,12 @@ for($i = 0; $i < scalar @wlines; $i++)
 
 print "$numusers users found\n";
 
+#for each user, create data structure
 for($i = 0; $i < $numusers; $i++)
 {
     $temp1 = $wlines[$i+2];
     if($temp1 =~ /^(\w+)\s*([\w\/]+)/)#\s*:?\w?\s*(\d{,2}\w{3}\d\d?)\s*[\w:]*\s*[\w:]*\s*[\w:]*\s*(\w.*$)/)
+    #regex => (beginning)-word-space-word
     {
         $userdata{'name'} = $1;
         $userdata{'tty'} = $2;
@@ -92,7 +99,7 @@ for($i = 0; $i < $numusers; $i++)
     @userpids = ();
     foreach $process (@psttydata)
     {
-        if($process =~ /\w+\s+(\d+)/)
+        if($process =~ /\w+\s+(\d+)/) #regex => word-space-number
         {
             chomp($1);
             push @userpids, $1;
@@ -105,14 +112,17 @@ for($i = 0; $i < $numusers; $i++)
 ################################
 # DETERMINE WHAT TTY THIS PROGRAM IS RUNNING ON
 ################################
-open MYTTY, "tty |"; $mytty = <MYTTY>; close MYTTY; $mytty =~ s/^\/dev//; 
+open MYTTY, "tty |";
+$mytty = <MYTTY>;
+close MYTTY;
+$mytty =~ s/^\/dev//; 
 
 #can't literally have an array of hashes
 #can have an array of references to hashes
 #[] and {} are array and hash constructors respectively. they are guarunteed to allocate new memory
 
 #########################
-# PRINT OUT
+# PRINT OUT ALL USERS
 #########################
 
 print "Output from w\n";
@@ -120,31 +130,147 @@ print "====================\n";
 for($i = 0; $i < $numusers; $i++)
 {
     $temp2 = $users[$i];
+    #for each element of the selected user
     foreach $key (keys (%$temp2))
     {
+        #make sure it's not the pid list
         if($key !~ /pidlist/)
         {
+            #print selected element
             print $temp2->{$key}.",";
         }
     }
+    
+    #if the terminal was the user's terminal (THIS terminal)
+    if($temp2->{'tty'} =~ $mytty)
+    {
+        print "***";
+    }
+    
     print "\n";
     
     $temp1 = $temp2->{'pidlist'};
+    #for each process belonging to this user
     foreach $process (@$temp1)
     {
         if(defined($process) && length($process))
         {
-            print "\t-$process\n";
+            print "\t";
+            #print process name
+            system("ps $process | grep $process");
         }
     }
+    print "\n";
 }
 
-########################################
+#########################
 # SETUP COMMAND MENU
-########################################
+#########################
 
-#pid (list), tty (list), user, allexceptme, all
+print "\nEnter a process number to kill a process.\n";
+print "Enter a set of process numbers separated by spaces to kill those processes.\n";
+print "Enter the tty path to kill all the processes with that tty.\n";
+print "Enter \"user<name>\" to kill all processes and ttys under the user <name>.\n";
+print "Enter \"allusers\" to kill all users (including yourself).\n";
+print "Enter \"allothers\" to kill all users (except yourself).\n";
+print "Enter \"q\" to quit and do nothing.\n";
+print ":";
 
-#use "ps <pid> | grep <pid>"
+$cmd_choice = <STDIN>;
+
+#########################
+# Process sub-command
+#########################
+
+if($cmd_choice =~ /^\d+/) #regex => number
+{
+    print "killing processes $cmd_choice\n";
+    ### TODO: program in some sort of semaphore or global variable to provide a check on forking
+    &reliablekill( [ $cmd_choice ], 1);
+
+}
+elsif($cmd_choice =~ /^\/dev/)
+{
+    print "killing tty $cmd_choice\n";
+
+}
+elsif($cmd_choice =~ /^user(\w+)/)
+{
+    $cmd_choice =~ s/^user//;
+    print "killing user $cmd_choice\n";
+
+}
+elsif($cmd_choice =~ /^allusers/)
+{
+    print "killing all users\n";
+
+}
+elsif($cmd_choice =~ /^allothers/)
+{
+    print "killing all other users\n";
+
+}
+#else q is for do nothing and quit
+
+print "exiting...\n";
 
 exit(0);
+
+############################################################
+# kill a list of processes passed in and make sure that
+# they stay killed
+#
+# will only fork once for all pids
+#
+# arg0 - reference to array
+# arg1 - size of array
+############################################################
+sub reliablekill
+{
+    my $i;
+    my $childpid = 0;
+    my @local_list;
+    my @lines;
+    my $list_ref = $_[0];
+    my $size = $_[1];
+    
+    for($i = 0; $i < $size; $i++)
+    {
+        unshift @local_list, $list_ref->[$i];
+        system("kill $local_list[0]");
+    }
+    
+    #if child then fork returns 0
+    #if parent then fork returns child id
+    $childpid = fork();
+    
+    #return if parent
+    if($childpid)
+    {
+        return;
+    }
+    else
+    {
+        sleep(5); #5 seconds
+        
+        for($i = 0; $i < $size; $i++)
+        {
+            open PIDCHECK, "ps $local_list[$i] |";
+            @lines = <PIDCHECK>;
+            close PIDCHECK;
+            
+            foreach (@lines)
+            {
+                if($_ =~ /$local_list[$i]/)
+                {
+                    system("kill -KILL $local_lise[$i]");
+                }
+            }#for all lines
+        }#for all pids
+        
+        exit(0);
+    }
+    
+    return;
+            
+}
--- sendmail-wrapper.pl
+++ sendmail-wrapper.pl
@@ -4,61 +4,137 @@ use strict;
 use warnings;
 
 my $temp;
+my $sending_email_address = "no-reply\@hotmail.com";
+my $recipient_email_address;
+my $message_file;
+my $force = 0;
+my $response;
 my @lines_out;
 my @temp_lines;
 
-if($#ARGV > -1 && $ARGV[0] =~ /-h/)
+#TODO: need to make sure that sendmail is install and running
+
+#TODO: delete temporary files after message has been sent
+
+#################################
+# PARSE ARGUMENTS IF ANY
+#################################
+
+for(my $i = 0; $i <= $#ARGV; $i++)
 {
-    print "this is the help message\n";
+    #help
+    if($ARGV[$i] =~ /-h/)
+    {
+        printhelp();
+        exit(0);
+    }
+    #force
+    elsif($ARGV[$i] =~ /-f/)
+    {
+        $force = 1;
+    }
+    #to <email addr>
+    elsif($ARGV[$i] =~ /-t/ && $#ARGV > $i)
+    {
+        $i++;
+        $recipient_email_address = $ARGV[$i];
+    }
+    #message file
+    elsif($ARGV[$i] =~ /-m/ && $#ARGV > $i)
+    {
+        $i++;
+        $message_file = $ARGV[$i];
+    }
+    #unknown
+    else
+    {
+        print "Unknown arguments. Use -h to learn acceptable arguments\n";
+        exit(-1);
+    }
 }
 
-push @lines_out, "From: no-reply\@post.net\n";
+#################################
+# SETUP EMAIL HEADER
+#################################
 
-print STDOUT "enter destination address:\n";
-$temp = <STDIN>;
-chomp $temp;
-push @lines_out, "To: $temp\n";
+push @lines_out, "From: $sending_email_address\n";
+
+#was an address passed in as a parameter?
+if(!(defined($recipient_email_address) && length($recipient_email_address) > 0))
+{
+    print STDOUT "enter destination address:\n";
+    $temp = <STDIN>;
+    chomp $temp;
+    push @lines_out, "To: $temp\n";
+}
+else
+{
+    push @lines_out, "To: $recipient_email_address\n";
+}
 
 push @lines_out, "Subject: Automatic E-mail\n";
 push @lines_out, "MIME-Version: 1.0\n";
 push @lines_out, "Content-Type: text/plain\n";
 push @lines_out, "\n";
 
-#set up comments for message file
-$temp = "\n".
-        "#lines beginning with an ampersand are comments and\n".
-        "#will not be printed\n".
-        "#\n".
-        "# 1. Edit this file\n".
-        "# 2. Save the file\n".
-        "# 3. Quit\n";
-open TEMPFILE, ">/tmp/sendmail-wrapper.usrmsg";
-print TEMPFILE $temp;
-close TEMPFILE;
+#################################
+# SETUP MAIN MESSAGE
+#################################
 
-#execute vim
-system "vim /tmp/sendmail-wrapper.usrmsg";
-
-#grab message
-open FINISHEDFILE, "/tmp/sendmail-wrapper.usrmsg";
-@temp_lines = <FINISHEDFILE>;
-close FINISHEDFILE;
-
-#parse message
-for(my $i = 0; $i <= $#temp_lines; $i++)
+#was a message file passed in as a paramater?
+if(!(defined($message_file) && length($message_file) > 0))
 {
-    $temp = $temp_lines[$i];
+    #set up comments for message file
+    $temp = "\n".
+            "#lines beginning with an ampersand are comments and\n".
+            "#will not be printed\n".
+            "#\n".
+            "# 1. Edit this file\n".
+            "# 2. Save the file\n".
+            "# 3. Quit\n";
+    open TEMPFILE, ">/tmp/sendmail-wrapper.usrmsg";
+    print TEMPFILE $temp;
+    close TEMPFILE;
 
-    if($temp !~ /^#/)
+    #execute vim
+    system "vim /tmp/sendmail-wrapper.usrmsg";
+
+    #grab message
+    open FINISHEDFILE, "/tmp/sendmail-wrapper.usrmsg";
+    @temp_lines = <FINISHEDFILE>;
+    close FINISHEDFILE;
+
+    #parse message
+    for(my $i = 0; $i <= $#temp_lines; $i++)
     {
-	chomp $temp;
-        push @lines_out, "$temp\n";
+        $temp = $temp_lines[$i];
+
+        if($temp !~ /^#/)
+        {
+        chomp $temp;
+            push @lines_out, "$temp\n";
+        }
     }
 }
+#################################
+# CREATE TEMP FILE TO SEND
+#################################
 
 open OUTFILE, ">/tmp/sendmail-wrapper.outmsg";
 print OUTFILE @lines_out;
 close OUTFILE;
+
+#do we skip the confirmation check?
+if(!$force)
+{
+    print "message ready to send. do you want to continue? (y/n):";
+    $response = <STDIN>;
+    if($response =~ /[Nn]/)
+    {
+        print "exiting.  message not sent\n";
+        exit(0);
+    }
+}
 
 print "sending message. this may take a few minutes\n";
 system "sendmail -i -t </tmp/sendmail-wrapper.outmsg";
@@ -66,3 +142,35 @@ system "sendmail -i -t </tmp/sendmail-wrapper.outmsg";
 #usually used when sending a pre-written file
 
 print "sent message\n";
+exit(0);
+
+############################################################
+# subroutine
+#
+# prints the help message
+#
+# no arguments and no returns
+############################################################
+sub printhelp
+{
+    print "$0 - steps through the process of using sendmail on outgoing messages\n\n";
+    
+    print "Usage: $0 [-h] [-f] [-t <dest address>] [-m <message file>]\n\n";
+    
+    print "if no arguments are passed in, the program will prompt the user for all";
+    print " of the necessary details.  The program defaults the sending email address";
+    print " to be $sending_email_addr.  The program will set the subject to \"Automatic";
+    print " E-mail\".\n\n"
+    
+    print "-t <dest address> - pass in a destination address (the -t stands for ";
+    print "the TO field)\n";
+    print "-m <message file> - pre-existing message file that will be sent (the -m";
+    print "stands for the MESSAGE field)\n";
+    print "-f                - Force option. Will skip the final confirmation that's";
+    print "prompted just before the message is sent\n";
+    print "-h                - Display help message and exit\n\n";
+    
+    print "Made by Steven Keller LeBlanc. 2014.\n"
+    print "\n";
+    return;
+}
