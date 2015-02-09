bbdbf6502f5f35d10d3a21b43120b92f78b7ef1e
Steven LeBlanc <skleblan@gmail.com>
2015-02-08 20:47:20 -0600

    Adds mail archiving script
    This script is being developed on SDF.  It gets copied over to this
    repo b/c there's no git support (that i have access to) on SDF.
    
    This utility will go through my messages folder and archive unsorted
    messages that are older that a certain date into "archive" folders.
    
    I suppose this could be made to work inside the sorted messages'
    folders as well.

---
+++ archive_older_mail.pl
@@ -0,0 +1,82 @@
+#!/usr/pkg/bin/perl
+
+# usage:
+# archive_older_mail.pl arg1 arg2 arg3 etc...
+
+use warnings;
+use strict;
+
+my @msg_file_names;
+my $ls_output;
+my @lines;
+my $line;
+my $cur_msg_file_name;
+my %frmtd_dates;
+
+##################################
+# get list of all emails
+##################################
+@msg_file_names;
+$ls_output = `ls ~skleblan/messages/`;
+@lines = split $ls_output; #not providing a delimeter breaks on whitespace
+for $line ( @lines )
+{
+  chomp $line;
+  #test if not null or empty
+  if( defined $line && $line neq "" )
+  {
+    #also check if not a directory ( -f is for file, -d is for directory )
+    if( -f $line == 1 )
+    {
+      push @msg_file_names, $line;
+      print "added $line to the list of files\n";
+    }
+  }
+  else
+  {
+    print "$line is not a file\n";
+  }
+}
+
+if( $#msg_file_names  < 0 )
+{
+  print "error: did not find any files\n";
+  exit(-1);
+}
+
+##################################
+# get dates of all emails
+##################################
+#$cur_msg_file_name
+
+open CUR_MSG, $cur_msg_file_name;
+@lines = <CUR_MSG>;
+close CUR_MSG;
+
+for $line ( @lines )
+{
+#  if( $line =~ /^Date:\s/ )
+#  {
+#    $hash_list{$cur_msg_file_name} = $line;
+#    break;
+#  }
+  if( $line =~ /From\s\S+\@\S\s\w{3}\s---/ )
+  {
+    $frmtd_dates{$cur_msg_file_name} = $line;
+    break;
+  }
+}
+
+######################################
+# call a subroutine fo format the dates
+######################################
+
+##################################
+# create folders foreach month of each year
+# in a directory called archive
+##################################
+
+##################################
+# but only create a directory if there's a message
+# to put in it
+##################################
