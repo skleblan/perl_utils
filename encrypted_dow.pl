b478cbbcaac1d48f7203a7a01c0acdafbe77c469
Steven LeBlanc <skleblan@gmail.com>
2017-06-30 12:04:25 -0500

    Fixed bug with encryption command. Successful test.
    Fixed a bug where we weren't specifying the recipient.
    Also installed GnuPG and generated a new GPG key for laptop2.
    Both are required for this program to run.

--- encrypted_dow.pl
+++ encrypted_dow.pl
@@ -4,13 +4,10 @@ use GnuPG qw( :algo );
 
 my $gpg = new GnuPG();
 
-print "Enter your passphrase for encryption:\n";
-my $secret = <STDIN>;
-
 #my $input_text = `date +%A`;
 system("date +%A > today.tmp");
 
 $gpg->encrypt( plaintext => "today.tmp", output => "today.gpg",
-    passphrase=>$secret);
+    recipient=>"steven\@laptop2");
 
 print "Done\n";
