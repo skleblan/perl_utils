5da86279f8a685a60952993099f9f9a5973a3de3
Steven LeBlanc <skleblan@gmail.com>
2018-07-09 21:14:53 -0500

    Copy dirs with slash. Tested Extra Files
    Original test specified directories without the
    trailing slash and it copied the folder in
    addition to the files.
    
    Added test that included the trailing slash, and
    only the files inside were copied.
    
    Added tests where there was an extra file in the
    destination folder. It was not removed during the
    rsync call.

--- rsync_test.pl
+++ rsync_test.pl
@@ -15,9 +15,14 @@ if(not -e $rootdir)
 die "cannot create sandbox root directory\n" unless -d $rootdir;
 chdir $rootdir;
 
-
 &run_test_01();
+&run_test_02();
 
+#test un-recognized files in destination
+&run_test_03();
+&run_test_04();
+
+#investigate --bwlimit for rsync
 
 exit(0);
 
@@ -42,4 +47,72 @@ sub run_test_01
   #-a uses the archive option which is a combination of -r, -l, -p, -t,
   #    -g, -o, -D.  This is a very useful option
   #-v uses the verbose mode.
+}
+
+sub run_test_02
+{
+  my $test_dir_src = "test_02_1_src";
+  my $test_dir_dest = "test_02_2_dest";
+  mkdir $test_dir_src unless -e $test_dir_src;
+  mkdir $test_dir_dest unless -e $test_dir_dest;
+
+  chdir $test_dir_src;
+
+  foreach("alpha", "bravo", "charlie")
+  {
+    system "echo $_ > $_.txt";
+  }
+
+  chdir $rootdir;
+  #Currently, this copies the individual files to the destination without
+  #the enclosing folder
+  system("rsync -av $test_dir_src/ $test_dir_dest/");
+}
+
+sub run_test_03
+{
+  my $test_dir_src = "test_03_1_src";
+  my $test_dir_dest = "test_03_2_dest";
+  mkdir $test_dir_src unless -e $test_dir_src;
+  mkdir $test_dir_dest unless -e $test_dir_dest;
+
+  chdir $test_dir_src;
+
+  foreach("alpha", "bravo", "charlie")
+  {
+    system "echo $_ > $_.txt";
+  }
+  chdir $rootdir;
+  chdir $test_dir_dest;
+  print "test 03: creating delta.txt in destination folder\n";
+  system "echo delta > delta.txt";
+
+  chdir $rootdir;
+  #Currently, this copies the individual files to the destination without
+  #the enclosing folder
+  system("rsync -av $test_dir_src $test_dir_dest");
+}
+
+sub run_test_04
+{
+  my $test_dir_src = "test_04_1_src";
+  my $test_dir_dest = "test_04_2_dest";
+  mkdir $test_dir_src unless -e $test_dir_src;
+  mkdir $test_dir_dest unless -e $test_dir_dest;
+
+  chdir $test_dir_src;
+
+  foreach("alpha", "bravo", "charlie")
+  {
+    system "echo $_ > $_.txt";
+  }
+  chdir $rootdir;
+  chdir $test_dir_dest;
+  print "test 03: creating delta.txt in destination folder\n";
+  system "echo delta > delta.txt";
+
+  chdir $rootdir;
+  #Currently, this copies the individual files to the destination without
+  #the enclosing folder
+  system("rsync -av $test_dir_src/ $test_dir_dest/");
 }
