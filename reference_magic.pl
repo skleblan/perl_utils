4fa819fb0f39cd523ec0e8a5bbbe2da502f979fc
Steven LeBlanc <skleblan@gmail.com>
2018-09-16 19:34:05 -0500

    Basic URI Testing. Add Data::Dumper to ref magic
    Added the examples statements that exist in the URI perldoc entry.
    Still need to do more practice to figure out how to manipulate URIs
    on my own
    
    Added contextual labels to my print data for reference magic.
    Added my first use of Data::Dumper to reference magic to verify
    my complex data structure.

--- reference_magic.pl
+++ reference_magic.pl
@@ -1,21 +1,24 @@
 #!/usr/bin/perl
 use strict;
 use warnings;
+use Data::Dumper;
 
 my @plain = ("yogurt", "jane", "boeing");
 my $spicy = ["salsa", "peppers", "wings"];
 
-print @plain;
-print $spicy;
+print "plain: ".@plain."\n";
+print "spicy: ".$spicy."\n";
 
 my $name = \$plain[1];
 my $buffalo = ${$spicy}[2];
 
-print ref $name;
-print ref $buffalo;
+print "ref name (true. is reference): ".(ref $name)."\n";
+print "ref buffalo (false. is direct scalar): ".(ref $buffalo)."\n";
 
 my $complex = {};
 
 $complex->{bill} = "wild";
 $complex->{months} = ["jan", "feb"];
 push @{$complex->{months}}, "mar", "apr";
+
+print "Dump: ".Dumper($complex)."\n";
---
+++ urimagic.pl
@@ -0,0 +1,25 @@
+#!/usr/bin/perl
+use URI;
+# https://en.m.wikipedia.org/wiki/McKinley_Tariff
+# https://en.m.wikipedia.org/wiki/Nobel_Prize_in_Physics
+# https://en.m.wikipedia.org/wiki/Europe
+# https://en.m.wikipedia.org/wiki/Raid_(military)
+# https://en.m.wikipedia.org/wiki/Draft_(hull)
+# https://en.m.wikipedia.org/wiki/Panama_Canal
+# https://en.wikipedia.org/wiki/Panama_Canal
+# https://en.m.wikipedia.org/wiki/Coffee
+# https://en.m.wikipedia.org/wiki/Iron_Man
+
+$u1 = URI->new("http://www.perl.com");
+$u2 = URI->new("foo", "http");
+$u3 = $u2->abs($u1);
+
+print "u1 $u1\n";
+print "u2 $u2\n";
+print "u3 $u3\n";
+
+print "scheme u3 ".$u3->scheme."\n";
+print "opaque u3 ".$u3->opaque."\n";
+print "path u3 ".$u3->path."\n";
+print "fragment u3 ".$u3->fragment."\n";
+
