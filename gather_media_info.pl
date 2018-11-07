33bd330e55d5446dcac90841cb7bd07450d655e0
Steven LeBlanc <skleblan@gmail.com>
2018-11-07 01:15:04 -0600

    Exif-tag script now produces single results file
    Modified gather_media_info.pl so that it returns some data objects
    (list of lists).  This way, all the information can be compiled into
    one master file for the root directory.
    
    Also modified the program to put in placeholder text when a file
    doesn't have one of the desired tags.

--- gather_media_info.pl
+++ gather_media_info.pl
@@ -5,12 +5,16 @@ use strict;
 
 use Image::ExifTool qw(:Public);
 use Data::Dumper;
+use File::Spec;
 use Cwd ();
 
 die "requires a path as an argument\n" unless scalar(@ARGV)>0;
 my $path = shift @ARGV;
 die "path doesnt exist\n" unless -e $path;
 die "path is not a directory\n" unless -d $path;
+
+my $outfile = "exif.csv";
+my $masterfile = 1;
 
 my @mytaglist = (
           'FileName',
@@ -25,7 +29,8 @@ my @mytaglist = (
           'TrackDuration',
           'MediaDuration',
           'Duration',
-          'AudioFormat'
+          'AudioFormat',
+          '__dir__'
 );
 
 sub filterdesiredtags
@@ -44,6 +49,7 @@ sub filterdesiredtags
 
 sub iteratedir
 {
+  my $curdir_file_tag_list = [];
   my $curdir = shift;
   die "not a directory\n" unless -d $curdir;
   my $oldpath = Cwd::cwd;
@@ -58,8 +64,13 @@ sub iteratedir
   }
   closedir DIRH;
  
-  my $outfile = "exif.csv";
-  open OUTH, ">$outfile" or die $!;
+  if(not $masterfile)
+  {
+    open OUTH, ">$outfile" or die $!;
+
+    print OUTH join(",", @mytaglist); #headers
+    print OUTH "\r\n";
+  }
  
   foreach (@diritems)
   {
@@ -67,7 +78,8 @@ sub iteratedir
     if(-d $_)
     {
       print $_." dir\n";
-      iteratedir($_);
+      my $sub_lol = iteratedir($_);
+      push @$curdir_file_tag_list, @$sub_lol;
       next;
     }
     next if not -f $_;
@@ -76,19 +88,48 @@ sub iteratedir
     #print "processing $_\n";
     my $taglist = ImageInfo($_);
     filterdesiredtags($taglist);
+    $taglist->{'__dir__'} = Cwd::cwd;
     my @vals = ();
     foreach my $t (@mytaglist)
     {
       if(defined $taglist->{$t})
-      { push @vals, $taglist->{$t}; }
+      {
+        $taglist->{$t} =~ s/,//;
+        push @vals, $taglist->{$t};
+      }
+      else { push @vals, "__undefined__"; }
     }
     my $tempstr = join(",", @vals);
-    print OUTH $tempstr."\r\n";
+    push @$curdir_file_tag_list, \@vals;
+    if(not $masterfile)
+    {
+      print OUTH $tempstr."\r\n";
+    }
   }
 
-  close OUTH;
+  if(not $masterfile)
+  {
+    close OUTH;
+  }
   print "changing to $oldpath\n";
   chdir $oldpath;
+
+  return $curdir_file_tag_list;
 }
 
-iteratedir($path);
+my $total_lol = iteratedir($path);
+
+if($masterfile)
+{
+  my $fullpath = File::Spec->catfile($path, $outfile);
+  open MASTER, ">$fullpath" or die $!;
+  print MASTER join(",", @mytaglist)."\r\n";
+
+  foreach my $big (@$total_lol)
+  {
+    my $tempstr = join(",", @$big);
+    print MASTER $tempstr."\r\n";
+  }
+
+  close MASTER;
+}
