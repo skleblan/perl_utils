8275b6525ecdab3bb7b8e72e3fc99eeece58b8a6
Steven LeBlanc <skleblan@gmail.com>
2018-11-28 13:27:55 -0600

    Finished attach-msmtp with bugs
    program is complete but not functional in
    every circumstance. doesnt preserve the filenames
    also, iphone has issues figuring out the filetype
    
    currently will not work for sending certificates to
    the iphone
    
    however, successful practice using GetOpt::Long.

--- attach-msmtp.pl
+++ attach-msmtp.pl
@@ -3,4 +3,29 @@ use strict; use warnings;
 use Email::Stuffer;
 use Getopt::Long;
 
+my $to = "";
+my $subj = "";
+my $filepath = "";
+
+GetOptions( "to=s" => \$to,
+  "subj=s" => \$subj,
+  "file=s" => \$filepath )
+  or die "error parsing arguments\n";
+
+die "bad email address\n" unless $to =~ /\w+\@\w+/;
+die "no subject\n" unless length $subj > 0;
+die "file doesnt exist\n" unless -e $filepath;
+die "not a file\n" unless -f $filepath;
+
 my $mime_msg = Email::Stuffer->new;
+$mime_msg->to($to);
+$mime_msg->subject($subj);
+$mime_msg->attach_file($filepath);
+
+my $tmpfile = "tmp.msg";
+open TMP, ">$tmpfile";
+print TMP $mime_msg->as_string;
+close TMP;
+
+(system ("msmtp -a gmail $to <$tmpfile") == 0)
+  or die "problem sending message with msmtp: $?\n";
