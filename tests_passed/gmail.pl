f9b4279471f270eed6934576c5840ec8a6aa0cd1
Steven LeBlanc <skleblan@gmail.com>
2018-11-30 13:32:14 -0600

    Gmail perl module passed
    Created first test with Email::Send::SMTP::Gmail.
    Seems to work for basic communication. Also
    did more practice with Getopt::Long.
    
    Created gitignore file to ignore config files that might have sensitive info.
    
    Created a data folder for storing simple test data. Added a small image file.
    
    Moved attach-msmtp into unknown. It will not function as desired for iphone mail, but it might work with other types of mailbox applications.

---
+++ tests_passed/gmail.pl
@@ -0,0 +1,31 @@
+#!/usr/bin/perl
+use strict; use warnings;
+
+use Getopt::Long;
+use Email::Send::SMTP::Gmail;
+
+my $password = "";
+my $from = "";
+my $to = "";
+die "bad args\n" unless scalar(@ARGV) == 1;
+GetOptions("to=s" => \$to) or die "bad args\n";
+
+open CFG, "gmail.cfg" or die $!;
+$from = <CFG>;
+$password = <CFG>;
+close CFG;
+
+chomp $from;
+chomp $password;
+
+my ($mail) = Email::Send::SMTP::Gmail->new(
+    -login=>$from,
+    -pass=>$password);
+
+my $subj = "Perl Gmail Test";
+my $body = "Can you find Maven the Raven?";
+
+$mail->send(-to=>$to, -from=>$from,
+  -subject=>$subj, -body=>$body);
+
+$mail->bye;
