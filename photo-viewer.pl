6a1721705000355adf2d66e5138ba31e9afa7385
Steven LeBlanc <skleblan@gmail.com>
2019-01-02 17:53:19 -0600

    Got photo-viewer working
    was able to successfully shrink the loaded image.

--- photo-viewer.pl
+++ photo-viewer.pl
@@ -2,12 +2,16 @@
 
 use strict; use warnings;
 use Tk;
+#use Tk::widgets qw/JPEG PNG TIFF/;
+#need to be explicitly imported??
+#although this current test uses a GIF, which is fine
 
 my $mw = MainWindow->new;
 $mw->Label(-text=>"Image Viewer 01")->pack;
 my $img = $mw->Photo(-file=>"/home/steven/git/utils/data/trees.gif");
-#my $newimg = $mw->Photo;
-$img->copy($img, -subsample=>(2,2));
-$mw->Label(-image=>$img)->pack;
+my $smallimg = $mw->Photo();
+$smallimg->copy($img, -subsample=>(2,2));
+#if trying to shrink image, cannot use same source and destination
+$mw->Label(-image=>$smallimg)->pack;
 
 MainLoop;
