be250353efa41ec1c91e196f5eb020fc0dac5a0d
Steven LeBlanc <skleblan@gmail.com>
2021-02-05 17:15:02 -0600

    Try to improve image hashing functions (fail)
    Added functions for Avg-Hash and Diff-Hash (from same CPAN module)
    
    tried to add functionality to take different sized hashes.
    kept getting errors that originated from inside the module itself

--- imgchk/imgchk.pm
+++ imgchk/imgchk.pm
@@ -29,12 +29,23 @@ sub is_same
 {
   my $file1 = shift;
   my $file2 = shift;
+  my $hash_size = 6;
 
   my $retval = &_is_same_checksum($file1, $file2);
 
+  if(0)
+  {
+    $retval = &_is_same_ahash($file1, $file2, $hash_size);
+  }
+
+  if(0)
+  {
+    $retval = &_is_same_dhash($file1, $file2, $hash_size);
+  }
+
   if(not $retval)
   {
-    $retval = &_is_same_phash($file1, $file2);
+    $retval = &_is_same_phash($file1, $file2, $hash_size);
   }
 
   if(not $retval)
@@ -70,16 +81,55 @@ sub _is_same_phash
 {
   my $file1 = shift;
   my $file2 = shift;
+  my $size = shift;
+  print "size is $size\n";
 
   my $img = read_file($file1, binmode => ':raw');
   my $ihash1 = Image::Hash->new($img, "GD");
-  my $phash1 = $ihash1->phash;
+  my $geometry = "".$size."x".$size;
+  my $phash1 = $ihash1->phash('geometry' => $geometry);
 
   $img = read_file($file2, binmode => ':raw');
   my $ihash2 = Image::Hash->new($img, "GD");
-  my $phash2 = $ihash2->phash;
+  my $phash2 = $ihash2->phash('geometry' => $geometry);
 
   return ($phash1 eq $phash2);
+}
+
+sub _is_same_ahash
+{
+  my $file1 = shift;
+  my $file2 = shift;
+  my $size = shift;
+
+  my $img = read_file($file1, binmode => ':raw');
+  my $ihash1 = Image::Hash->new($img, "GD");
+  my $geometry = "".$size."x".$size;
+  my $ahash1 = $ihash1->ahash('geometry' => $geometry);
+
+  $img = read_file($file2, binmode => ':raw');
+  my $ihash2 = Image::Hash->new($img, "GD");
+  my $ahash2 = $ihash2->ahash('geometry' => $geometry);
+
+  return ($ahash1 eq $ahash2);
+}
+
+sub _is_same_dhash
+{
+  my $file1 = shift;
+  my $file2 = shift;
+  my $size = shift;
+
+  my $img = read_file($file1, binmode => ':raw');
+  my $ihash1 = Image::Hash->new($img, "GD");
+  my $geometry = "".$size."x".$size;
+  my $dhash1 = $ihash1->dhash('geometry' => $geometry);
+
+  $img = read_file($file2, binmode => ':raw');
+  my $ihash2 = Image::Hash->new($img, "GD");
+  my $dhash2 = $ihash2->dhash('geometry' => $geometry);
+
+  return ($dhash1 eq $dhash2);
 }
 
 sub _is_same_sift
